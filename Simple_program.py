c31ed1637a4f16d6cb347126cd6363e6bdb0deb8
mahidrdo <mkarya.delhi@gmail.com>
2025-08-04 18:51:19 +0530

    Add files via upload

---
+++ Simple_program.py
@@ -0,0 +1,25 @@
+
+#Assignment Task 1
+a=input('Enter First Number:')
+a=int(a)
+b=input('Enter Second Number:')
+b=int(b)
+print('Enter your First Number:',a)
+print('Enter your Second Number:',b)
+print('sum of two number:',a+b)
+print('Difference of two number:',a-b)
+print('Multiplication of two number:',b*a)
+print('Division of two number:',a/b)
+
+
+#Assignment Task 2
+a=input('Enter your first name:')
+b=input('Enter your last name:')
+c=a+" "+b
+print("Hello,\t" +c+  "!\twelcome to the python program")
+
+#f-string Method
+a=input('Enter your first name:')
+b=input('Enter your last name:')
+c=a+" "+b
+print(f"Hello\t{c}!\twelcome to the python program")
